485b35abc17d4d93b87093e0e4c029e4a28aa9d8
opilane <opilane@HP-CZC6297BB1>
2022-01-19 13:54:15 +0200

    dsadsdas

--- RusEngTranslate.py
+++ RusEngTranslate.py
@@ -5,7 +5,7 @@ rus_list=failist_lugemine("rus.txt",rus_list)
 eng_list=failist_lugemine("eng.txt",eng_list)
 
 while True:
-	menu=input("Kõik sõnad -S, \nTõlkida -T, \nUus sõna -U, \nViga -V, \nKontroll -K, \nLõpp -L \n")
+	menu=input("Rääkida -R, \nKõik sõnad -S, \nTõlkida -T, \nUus sõna -U, \nViga -V, \nKontroll -K, \nLõpp -L \n")
 	if menu.upper()=="U":
 		rus=uus_sona("rus.txt", input("Новое слово: "))
 		eng=uus_sona("eng.txt", input("New word: "))
@@ -16,4 +16,18 @@ while True:
 		tolkimine(rus_list,eng_list)
 	elif menu.upper()=="L":
 		print("Хорошо, тикаем")
+		break
+	elif menu.upper()=="R":
+		keel=input("Mis keels ütelda?")
+		sonad=""
+		if keel=="ru":
+			mas=rus_list
+			lang="ru"
+		else:
+			mas=est_list
+			lang="eng"
+		for sona in mas:
+			sonad=sonad+" "+sona
+		heli(sonad,lang)
+	else:
 		break
